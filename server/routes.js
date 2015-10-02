2592a29dcddba3e5c8d1ef9ce1cc8cbbd7e426f9
David Unkefer <dave@forepoint.com>
2015-10-02 07:13:25 -0500

    -

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,7 @@ var weatherURI = "http://api.openweathermap.org/data/2.5/weather?q=";
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
+  //app.use('/api/things', require('./api/thing'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
